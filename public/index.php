9a5cd4e76f4f293ec89ec0be72f3650bf4f2f966
rodrigodenner <rodrigodenner.art@gmail.com>
2024-01-06 21:27:40 -0300

    feat: create method controller

--- public/index.php
+++ public/index.php
@@ -1,11 +1,14 @@
 <?php
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
+  return false;
+} else {
+  session_start();
+  define("DEFAULT_CONTROLLER", 'home');
+  define("DEFAULT_METHOD", 'index');
+
   require "../vendor/autoload.php";
   require "bootstrap/bootstrap.php";
   require "../App/Functions/functions_twig.php";
-
-  session_start();
-  const DEFAULT_CONTROLLER = 'home';
-  const DEFAULT_METHOD = 'index';
-
-
+}
 
